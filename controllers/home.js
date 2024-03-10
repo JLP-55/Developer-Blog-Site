29750afa74b0366f2cdd029da40dff9c54aab6e7
JLP-55 <jack.v.pekin@gmail.com>
2024-03-10 19:01:25 +1100

    try getting nodemailer to work in the hompage get route. it will be useful for the group project also made some minor changes and notes on what I think I should do next

---
+++ controllers/home.js
@@ -0,0 +1,6 @@
+// from here try to do the get requests for all the links in the header login/logout/dashboard/home
+// the user should be prompted to login or create an account before they can view the website 
+// upon creating an account, an email will be sent to the user to confirm their account creation
+const router = require("express").Router();
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,22 +2,43 @@ const express = require("express"); /*Express will be used to do all out routing
 
 const path = require("path"); /*The path parameter enables us to reach api endpoints.*/
 
+const routes = require("./controllers"); /*use this variable to access all routes*/
+
+// this will automatically run nodemailer function to send an email
+// you will have to route it correctly
+// const nodeMailer = require("./nodemailer/mail-app.js");
+
 // const session = require("express-session"); /*Express-session = middleware*/
 
 // const expressHandlebars = require("express-handlebars"); /*Template engine*/
 
-// const api = require("./controllers"); /*use this variable to access all routes*/
 	
 // const db = require("./config/connection"); /*connect to the database*/
 
 const app = express(); /*app = an instance of express*/ 
 const PORT = process.env.PORT || 3001; /*listen at PORT*/ 
 
+// app.use(routes);
+
 // middleware
 app.use(express.json());
 app.use(express.urlencoded ({extended: true}));
+app.use(routes);
+
 // serves up all static files, html, css, etc.
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// static assets for home
+app.get("/", (req, resp) => {
+	resp.sendFile(path.join(__dirname, "./public/html/index.html"));
+	console.log("viewing home page");
+});
+
+// static assets for login
+app.get("/login", (req, resp) => {
+	resp.sendFile(path.join(__dirname, "./public/html/login.html"));
+	console.log("viewing login page");
+});
 
 app.listen(PORT, () => {
 	console.log(`app is listening at http://localhost/${PORT}`);
