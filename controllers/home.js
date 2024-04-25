ed1a2bb360f5c11d6c73b9f4491423aad4bc12c3
JLP-55 <jack.v.pekin@gmail.com>
2024-04-25 11:18:43 +1000

    removed nodemailer changes

--- controllers/home.js
+++ controllers/home.js
@@ -4,7 +4,7 @@
 const router = require("express").Router();
 const path = require("path");
 
-const mail= require("nodeMailer");
+// const mail= require("nodeMailer");
 
 router.get("/", async (req, resp) => {
 	// try {
